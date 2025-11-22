f8b13cd08b38f26cbc343c52d6e014913d39da8e
taegyun <xorbskim6105@gmail.com>
2025-11-23 03:13:03 +0900

    fix(config) : 도메인 주소 추가

--- vite.config.js
+++ vite.config.js
@@ -7,5 +7,15 @@ export default defineConfig({
   resolve: {
     // .js와 .jsx 확장자를 생략할 수 있도록 추가
     extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json']
+  },
+  server: {
+    port: 5173, 
+    allowedHosts: [
+      'localhost', // 기존 허용
+      '127.0.0.1', // 기존 허용
+      'coupon.taegyunkim.com', 
+      '3.38.114.206' 
+    ],
+    // host: true, // 외부 접속을 허용하려면 주석 해제 (필요시)
   }
 });
