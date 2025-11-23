68401d574ba7d3cff288c4fe9ef5dc4d001d4598
taegyun <xorbskim6105@gmail.com>
2025-11-23 23:13:06 +0900

    fix(config) : 도메인 주소 수정

--- src/App.jsx
+++ src/App.jsx
@@ -61,7 +61,8 @@ const App = () => {
   
   // const IP_ADDRESS = "3.38.114.206";
 
-  const IP_ADDRESS = "localhost"; // 로컬 테스트용
+  // const IP_ADDRESS = "localhost"; // 로컬 테스트용
+  const IP_ADDRESS = "coupon.taegynkim.com";
   const memberRegisterAPI = useCallback(async (email) => {
     const API_URL = `http://${IP_ADDRESS}:8080/member/register`;
     
